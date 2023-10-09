f79ce03e6e7677de12ae5a44a3352548b1f1bc54
Marco <marco.porcaro@synesthesia.it>
2023-10-09 15:46:10 +0200

    send message

--- lib/repository/chat_repository.dart
+++ lib/repository/chat_repository.dart
@@ -1,13 +1,39 @@
 
+import 'package:chat/data/Message.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+
 class ChatRepository {
 
   static Future<bool> sendMessage(String text) async {
-    //recuperare l'utente loggato
-    //chiamare sdk di firebase realtime database e inviare il testo del messaggio
-    //e l'utente che lo manda
-    //ritornare l'esito dell'invio
+    FirebaseAuth auth = FirebaseAuth.instance;
+    final userEmail = auth.currentUser?.email;
 
-    return true;
+    if(userEmail == null) {
+      debugPrint("Utente non loggato");
+      return false;
+    }
+
+    try {
+      final message = Message(
+          text: text,
+          sender: userEmail);
+
+      final database = FirebaseDatabase.instance;
+      final messagesRef = database.ref("messages");
+      final messageRef = messagesRef.push();
+      await messageRef.set({
+        "text": message.text,
+        "sender": message.sender,
+      });
+      return true;
+    }
+    catch (e) {
+      debugPrint("Errore durante l'invio del messaggio");
+      return false;
+    }
+
   }
 
 }
